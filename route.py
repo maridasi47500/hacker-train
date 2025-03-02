b9f8dd0915c2a695d9d528e5d5b8965f6d8e6522
nave pirata <anonyme201504@gmail.com>
2025-03-02 00:43:52 -0300

    i am driver

--- route.py
+++ route.py
@@ -14,7 +14,7 @@ import sys
 
 class Route():
     def __init__(self):
-        self.Program=Directory("hacker ethique")
+        self.Program=Directory("hacker  du train ")
         self.Program.set_path("./")
         self.mysession={"notice":None,"email":None,"name":None}
         self.dbScript=Myscript()
